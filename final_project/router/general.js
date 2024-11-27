f0bbb8f00a577c1cbad5e3d2f870af95fd57bf4e
Steven Wheeler <steven@stevenewheeler.com>
2024-11-27 13:33:44 -0500

    Fixing promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -66,7 +66,7 @@ public_users.get("/get-books",function (req, res) {
 });
   
 // Get book details based on ISBN with get books ISBN promise
-public_users.get("/get-books-ISBN",function (req, res) {
+public_users.get("/get-books-ISBN/:isbn",function (req, res) {
     
     const isbn = req.params.isbn
     const get_books_ISBN = new Promise((resolve, reject) => {
@@ -83,7 +83,7 @@ public_users.get("/get-books-ISBN",function (req, res) {
 });
 
 // Get book details based on author with get books author promise
-public_users.get("/get-books-author",function (req, res) {
+public_users.get("/get-books-author/:author",function (req, res) {
     
     const get_books_author = new Promise((resolve, reject) => {
 
@@ -94,7 +94,7 @@ public_users.get("/get-books-author",function (req, res) {
         booksbyauthor.push({"isbn":isbn,
                             "title":books[isbn]["title"],
                             "reviews":books[isbn]["reviews"]});
-      resolve(res.send(JSON.stringify({ books }, null, 4)));
+      resolve(res.send(JSON.stringify({booksbyauthor}, null, 4)));
     }
 
     });
@@ -107,7 +107,7 @@ public_users.get("/get-books-author",function (req, res) {
 });
 
 // Get book details based on title with get books title promise
-public_users.get("/get-books-title",function (req, res) {
+public_users.get("/get-books-title/:title",function (req, res) {
     
     const get_books_title = new Promise((resolve, reject) => {
     let booksbytitle = [];
@@ -117,7 +117,7 @@ public_users.get("/get-books-title",function (req, res) {
         booksbytitle.push({"isbn":isbn,
                            "author":books[isbn]["author"],
                            "reviews":books[isbn]["reviews"]});
-      resolve(res.send(JSON.stringify({ books }, null, 4)));
+      resolve(res.send(JSON.stringify({booksbytitle }, null, 4)));
     }
 
     });
